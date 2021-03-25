1f17e6328edd62456ccd40705a40896aff2deadd
AngusLaurenson <asl203@exeter.ac.uk>
2021-03-25 17:11:26 +0000

    fixed __init__.py to import CmeansModel

--- fuzzy_water_clustering/__init__.py
+++ fuzzy_water_clustering/__init__.py
@@ -4,10 +4,10 @@
 #
 #
 # # make avilable directly from toplevel
-# # __all__ = ['predict_file','pipeline_to_xarray','sample_file','scoring']
 
 from .apply_model import predict_file
 from .serialize_models import pipeline_to_xarray
 from .sample_netcdf import sample_file
 
+from .cmeans_python import CmeansModel
 from . import cluster_scoring
