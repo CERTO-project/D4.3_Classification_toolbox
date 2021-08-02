dd27ed4a501e6404787401d8ce60d22953b7b9db
Angus Laurenson <anla@pmpc1531.home>
2021-08-02 17:10:12 +0100

    increment version to 0.1.0, changed name to fuzzy-water for deconfliction with new pypi.org package fuzzy_clustering

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ setup(
     # There are some restrictions on what makes a valid project name
     # specification here:
     # https://packaging.python.org/specifications/core-metadata/#name
-    name='fuzzy_clustering',  # Required
+    name='fuzzy_water',  # Required
 
     # Versions should comply with PEP 440:
     # https://www.python.org/dev/peps/pep-0440/
@@ -41,7 +41,7 @@ setup(
     # For a discussion on single-sourcing the version across setup.py and the
     # project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.0.9',  # Required
+    version='0.1.0',  # Required
 
     # This is a one-line description or tagline of what your project does. This
     # corresponds to the "Summary" metadata field:
