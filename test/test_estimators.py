8c46e3397a5d641eb9375b9ca269a8fcb8b7c9b7
Angus Laurenson <anla@pmpc1531.home>
2021-08-05 17:00:25 +0100

    added test_estimators.py it uses sklearn checks within pytest framework to test our estimators

---
+++ test/test_estimators.py
@@ -0,0 +1,44 @@
+'''
+pytest script for scikit-learn estimators
+
+AUTHOR:
+    Angus Laurenson, Plymouth Marine Laboratory
+    anla@pml.ac.uk
+
+DESCRIPTION:
+    This script tests estimators against scikit-learn
+    standards using sklearn.utils.estimator_checks. 
+    
+    It is designed for use with pytest and will be
+    automatically picked up by pytest command:
+
+USAGE:
+    # Call all test scrips using pytest, including
+    # this one:
+    >> pytest
+
+    # Specifcally call this script with pytest:
+    >> pytest test_estimator.py
+
+'''
+
+import pytest
+
+# setup logger
+import logging
+logger = logging.getLogger(name='CmeansModel_tester')
+
+# import the package locally to test
+import sys; sys.path.append("..")
+import fuzzy_water_clustering as fwc
+
+# import an extensive suite of checks
+# from scikit-learn
+from sklearn.utils import estimator_checks
+
+# list of estimators to check
+ESTIMATORS = [fwc.CmeansModel()]
+
+@estimator_checks.parametrize_with_checks(ESTIMATORS)
+def test_sklearn_compatible_estimator(estimator, check):
+    check(estimator)
